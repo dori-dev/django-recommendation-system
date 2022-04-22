8ee4c27be2010670c6354ba4f5e1ff7d011629e1
mohammad <mr.dori.dev@gmail.com>
2022-04-22 19:22:42 +0430

    add code generator

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "code", "recommended_by"]
+
+
+admin.site.register(Profile, ProfileAdmin)
---
+++ profiles/utils.py
@@ -0,0 +1,11 @@
+from django.utils.crypto import get_random_string
+
+
+def generate_code() -> str:
+    """generate the recommendation code
+
+    Returns:
+        str: random recommendation code
+    """
+    code = get_random_string(12)
+    return code
