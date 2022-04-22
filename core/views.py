20df01c9b12f576bcf2de580bc5a0afcfdad921d
mohammad <mr.dori.dev@gmail.com>
2022-04-22 21:19:14 +0430

    add index page

---
+++ core/views.py
@@ -0,0 +1,15 @@
+"""core views
+"""
+from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
+from profiles.models import Profile
+
+
+def index_view(request: WSGIRequest, code: str = None):
+    if code:
+        profile = Profile.objects.filter(code=code)
+        if profile.exists:
+            request.session['code'] = profile[0].id
+    print(request.session)
+    print(request.session.get_expiry_age())
+    return render(request, 'index.html', {})
