4cc878f716d020b35bb1f6087a8b79241405b86b
mohammad <mr.dori.dev@gmail.com>
2022-04-22 19:25:36 +0430

    add admin model for profile

--- profiles/admin.py
+++ profiles/admin.py
@@ -3,7 +3,9 @@ from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ["code"]
     list_display = ["user", "code", "recommended_by"]
+    list_filter = ["recommended_by"]
 
 
 admin.site.register(Profile, ProfileAdmin)
