f5d78537b25e3eca563f313640108cee0d859243
mohammad <mr.dori.dev@gmail.com>
2022-04-22 19:07:36 +0430

    add profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=256, blank=True)
+    code = models.CharField(max_length=12, blank=True)
+    recommended_by = models.ForeignKey(User,
+                                       on_delete=models.CASCADE,
+                                       blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def get_recommended_profiles(self):
+        pass
+
+    def save(self, *args, **kwargs):
+        if self.code == "":
+            pass
+        susper().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.user.username
